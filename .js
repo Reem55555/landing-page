29fd8d91299ab3a6c990ce9e9ad29e16f118b831
Reem55555 <105117948+Reem55555@users.noreply.github.com>
2022-05-07 17:15:41 +0200

    Add files via upload

---
+++ .js
@@ -0,0 +1,9 @@
+var season = prompt("what is the best season fo you");
+switch(season){
+    case "summer":
+    alert("this is soo hot");
+    break;
+    case "winter":
+    alert("wow good match");
+    break;
+}
